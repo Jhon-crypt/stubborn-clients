95261db69f7802f81a2684606864569b98e85986
Jhon-crypt <johnoladele690@gmail.com>
2024-08-19 21:37:13 +0100

    feat: Updated Layouts

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,6 @@ import BootstrapJsUtil from "./utils/bootstrapJs";
 import "./globals.css";
 import Head from "next/head"; 
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "Stubborn Clients",
   description: "For Devs With Stubborn Clients",
@@ -19,7 +17,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital@0;1&display=swap" rel="stylesheet" />
       </Head>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
       <BootstrapJsUtil />
     </html>
   );
